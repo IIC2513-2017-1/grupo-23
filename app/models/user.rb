dcc0910f670d8b0ac81461a77b731d37b206df24
lileiva <lileiva@uc.cl>
2017-04-30 23:47:30 -0300

    relaciones

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,6 @@ class User < ApplicationRecord
   belongs_to :team, optional:true
   has_many :user_invitations
   has_many :tournaments
-<<<<<<< HEAD
-  has_many :publication
-  has_many :comments
-  has_and_belongs_to_many :matches
-=======
   has_many :publications
   has_many :comments
   has_and_belongs_to_many :matches
