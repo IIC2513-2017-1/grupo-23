fbaec3e78a7bd7397ae87c3a628ce8f6a686ee75
Jorge Trincado <jatrincado@uc.cl>
2017-05-11 19:40:18 -0300

    Add login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user&.password == params[:session][:password]
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Ingreso exitoso.'
+    else
+      redirect_to(new_session_path, alert: 'Correo o clave incorrectas.')
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'Salida de sesión exitosa.'
+  end
+end
