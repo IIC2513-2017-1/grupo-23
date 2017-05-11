dfd4c311b98f14d1a1e21dda5ebaa922b2cd3bad
Jorge Trincado <jatrincado@uc.cl>
2017-05-11 19:44:15 -0300

    Dependent comments and image to publication

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,9 +1,9 @@
 class Publication < ApplicationRecord
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", dependent: :destroy
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates :content, presence: true, length: { minimum: 6, maximum: 500 },
    allow_blank: false
 
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
