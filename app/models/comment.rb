f16880e37afdd72cdc9ef3ca0665b83a6fef683b
Jorge Trincado <jatrincado@uc.cl>
2017-05-14 17:38:15 -0400

    Add nested comments to publications

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   validates :content, presence: true, length: { minimum: 1, maximum: 50 },
    allow_blank: false
 
-   belongs_to :user
- belongs_to :publication
+  belongs_to :user
+  belongs_to :publication
 end
