15aea9bb937359aec3764f6c12765a291f019ea3
Jorge Trincado <jatrincado@uc.cl>
2017-05-14 18:25:07 -0400

    Change comment controller redirects to publications_path

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   include Secured
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
   before_action :logged_in?
-  before_action :find_publication
+
 
   def index
     @comments = Comment.all
@@ -20,12 +20,9 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params)
-    @comment.publication_id = @publication.id
-    @comment.user_id = current_user.id
-
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to publications_path, notice: 'Comentario creado.' }
+        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
@@ -37,7 +34,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to publications_path, notice: 'Comentario editado.' }
+        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
@@ -49,7 +46,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to publications_path, notice: 'Comentario destruido.' }
+      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -58,11 +55,6 @@ class CommentsController < ApplicationController
     def set_comment
       @comment = Comment.find(params[:id])
     end
-
-    def find_publication
-      @publication = Publication.find(params[:publication_id])
-    end
-
     def comment_params
       params.require(:comment).permit(:content)
     end
