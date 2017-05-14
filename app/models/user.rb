5b88e30e34a6267a45177b81f9984add12b5d1c0
lileiva <lileiva@uc.cl>
2017-05-14 17:04:52 -0400

    Add files via upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   
-  validates :password, presence: true, length: { minimum: 6 },
-                       confirmation: true, allow_blank: false
   validates :email, presence: true, uniqueness: true, allow_blank: false,
                     format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
