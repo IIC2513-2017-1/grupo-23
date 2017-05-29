b61f3ed9a0f0256bbd569d8b1bd48c5400d8776d
Jorge Trincado <jatrincado@uc.cl>
2017-05-28 21:43:50 -0400

    Added general position table

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -2,9 +2,9 @@ class Tournament < ApplicationRecord
   validates :name, presence: true, uniqueness: true,
    length: { minimum: 1, maximum: 20 }, allow_blank: false
 
-   belongs_to :user
- has_many :tournament_invitations
+  belongs_to :user
+  has_many :tournament_invitations
   has_many :participates
- has_many :teams, through: :participates
- has_many :matches
+  has_many :teams, through: :participates
+  has_many :matches
 end
