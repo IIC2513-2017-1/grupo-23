436c6ad72f82ac35f5fa485c3e49e0a36e32254b
lileiva <lileiva@uc.cl>
2017-06-26 15:42:00 -0400

    bloqueo por autenticacion

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -1,5 +1,6 @@
 module Api::V1
   class MatchesController < ApiController
+    before_action :authenticate
     def show
       @match = Match.find(params[:id])
     end
--- app/controllers/api/v1/tournaments_controller.rb
+++ app/controllers/api/v1/tournaments_controller.rb
@@ -1,7 +1,7 @@
 module Api::V1
   class TournamentsController < ApiController
 
-
+    before_action :authenticate
     def index
       @tournaments = Tournament.all.order(:user_id)
     end
